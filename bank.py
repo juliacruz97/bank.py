94e77ceaab9a3129bdbb93a39bdb9c234b090a04
Julia Cruz@ <juliacruz97@hotmail.com>
2024-03-07 14:11:23 -0300

    Weekly Tasks 02

---
+++ bank.py
@@ -0,0 +1,5 @@
+'''Prompt the user and read in two money amounts (in cent)'''
+'''Add the two amounts'''
+'''Print out the answer in a human readable format with a euro sign and
+decimal point between the euro and cent of the amount''' 
+
